48566314987763a492c1dc7f8c5049a6fe959614
nonaka9682 <nonaka9862@WhiteBook.local>
2013-10-28 11:02:54 +0900

    Initial Commit

---
+++ HelloWorld2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld2
+//
+//  Created by HirokiNonaka on 2013/10/28.
+//  Copyright (c) 2013年 HirokiNonaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
